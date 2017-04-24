bb1578029f93477bbf76fe29e7b1771a89d1d428
Thomas1746 <thomas.fuge@gmail.com>
2017-04-24 22:56:15 +1200

    Cleaned code

--- src/main/java/net/pp/testengine/MusicManager.java
+++ src/main/java/net/pp/testengine/MusicManager.java
@@ -7,21 +7,15 @@ import java.io.IOException;
  * Created by Thomas on 23/04/2017.
  */
 public class MusicManager {
-    private Sound mainTrack;
     private Sound fightTrack;
     private Sound bangSound;
     private Sound pewSound;
     private Sound screechSound;
     private Sound popSound;
-    //private Sound track3;
-    //private Sound track4;
 
     public MusicManager(TestEngine engine) {
         try {
-            //mainTrack = new Sound(engine.dataFile("bgTrack3.wav"));
-            fightTrack = new Sound(engine.dataFile("bgTrack1.wav"));
-            /*track3 = new Sound(new File("bgTrack2.wav"));
-            track4 = new Sound(new File("bgTrack4.wav"));*/
+            fightTrack = new Sound(engine.dataFile("bgTrack1.wav")); // Sampled and modified "Crisis in the North," composed by Senju, Akira
             bangSound = new Sound((engine.dataFile(("gameSounds/bang.wav"))));
             pewSound = new Sound((engine.dataFile(("gameSounds/pew.wav"))));
             screechSound = new Sound((engine.dataFile(("gameSounds/screech.wav"))));
@@ -32,8 +26,6 @@ public class MusicManager {
     }
 
     public Sound getBangSound() { return bangSound; }
-
-    //public Sound getMainTrack() {return mainTrack; }
 
     public Sound getPewSound() { return pewSound; }
 
